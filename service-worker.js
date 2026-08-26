cff84e7bc16526da4a503736d33c4a4d9f1628f1
rahatshari <114852678+rahatshari@users.noreply.github.com>
2026-08-26 21:32:40 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'land-calculator-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});

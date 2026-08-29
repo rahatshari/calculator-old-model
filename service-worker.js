2661d74e0e255011a46983a7aa24f7e37e61183d
rahatshari <114852678+rahatshari@users.noreply.github.com>
2026-08-29 22:56:59 +0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,40 @@
-const CACHE_NAME = 'land-calculator-v3';
+const CACHE_NAME = 'land-calculator-offline-v1';
 const urlsToCache = [
-    './',
-    './index.html',
-    './manifest.json',
-    './icon.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // ক্যাশে থাকলে ক্যাশ থেকে দেবে, না থাকলে নেটওয়ার্ক থেকে আনবে
+      return response || fetch(event.request);
+    })
+  );
 });
